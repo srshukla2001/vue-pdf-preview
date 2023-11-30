62167c20e623eb618fcf4de508db3ec28f1ff40c
srshukla321 <srshukla321@gmail.com>
2023-11-30 12:50:59 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,102 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input v-model="pdfUrl" type="text" placeholder="Enter PDF URL" />
+    <button @click="showPdfPreview">Show Preview</button>
+
+    <div v-if="showPreview" class="popup-overlay" @click="closePreview">
+      <div class="popup" ref="popup">
+        <button @click="closePreview" class="close-btn">X</button>
+        <iframe :src="pdfUrl" width="800rem" height="700rem"></iframe>
+       
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      pdfUrl: '',
+      showPreview: false,
+      initialWidth: 0,
+      initialHeight: 0,
+      mouseX: 0,
+      mouseY: 0
+    };
+  },
+  methods: {
+    showPdfPreview() {
+      if (this.pdfUrl) {
+        this.showPreview = true;
+      }
+    },
+    closePreview() {
+      this.showPreview = false;
+    },
+    startResize(e) {
+      e.preventDefault();
+      this.isResizing = true;
+      this.initialWidth = this.$refs.popup.offsetWidth;
+      this.initialHeight = this.$refs.popup.offsetHeight;
+      this.mouseX = e.clientX;
+      this.mouseY = e.clientY;
+      document.addEventListener('mousemove', this.resize);
+      document.addEventListener('mouseup', this.stopResize);
+    },
+    resize(e) {
+      if (this.isResizing) {
+        const width = this.initialWidth + (e.clientX - this.mouseX);
+        const height = this.initialHeight + (e.clientY - this.mouseY);
+        this.$refs.popup.style.width = `${width}px`;
+        this.$refs.popup.style.height = `${height}px`;
+      }
+    },
+    stopResize() {
+      this.isResizing = false;
+      document.removeEventListener('mousemove', this.resize);
+      document.removeEventListener('mouseup', this.stopResize);
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.popup-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 200%;
+  height: 200%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+}
+
+.popup {
+  position: fixed;
+  top: 23rem;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  padding: 1px;
+  z-index: 1000;
+  overflow: auto;
+}
+
+.close-btn {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+}
+
+.resize-handle {
+  width: 10px;
+  height: 10px;
+  background-color: #000;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  cursor: nwse-resize;
 }
 </style>
